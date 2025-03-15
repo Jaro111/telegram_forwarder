a8dd9d072c6f6e7655e68919cdd32a104afc387c
Jaro111 <jarokark@protonmail.com>
2025-03-15 09:15:27 +0000

    changet to listen channel username

--- tg_fdr.py
+++ tg_fdr.py
@@ -4,33 +4,37 @@ from telethon import TelegramClient, events
 import asyncio
 from telegram import Bot
 
+
 load_dotenv()
 
 api_id = os.getenv('TG_API_ID')
 api_hash = os.getenv("TG_API_HASH")
 phone = os.getenv("PHONE")
 forward_bot_token=os.getenv("BOT_TOKEN")
+listen_chanel_username=os.getenv("LISTEN_CHANNEL_USERNAME")
 
 # Your bot credentials (for forwarding messages)
 bot_token = forward_bot_token  # From BotFather
 destination_chat_id = os.getenv("DESTINATION_ID")  # e.g., a user ID, group ID, or channel ID (like -100123456789)
 
 # Initialize Telethon client (user account)
-client = TelegramClient('/app/data/session_name', api_id, api_hash)
+client = TelegramClient('session_name', api_id, api_hash)
 
 # Initialize your Telegram bot
 bot = Bot(bot_token)
 
 # Define the private bot's chat ID or username
 private_bot_id = int(os.getenv("LISTEN_CHANNEL_ID"))  # Replace with the private bot's chat ID or username
+print(private_bot_id)
 
-@client.on(events.NewMessage(chats=private_bot_id))
+@client.on(events.NewMessage(chats=listen_chanel_username))
 async def handler(event):
     # Get the message content
+    print("bla")
     message_text = event.message.text
     print(f"Received from private bot: {message_text}")
-    exact_time = event.message.date  # UTC datetime with seconds
-    print(f"Received at {exact_time}")
+    # exact_time = event.message.date  # UTC datetime with seconds
+    # print(f"Received at {exact_time}")
 
     # Forward the message using your bot
     try:
@@ -38,7 +42,6 @@ async def handler(event):
         print(f"Forwarded to {destination_chat_id}")
     except Exception as e:
         print(f"Error forwarding message: {e}")
-        await asyncio.sleep(5)
 
 async def main():
     await client.start(phone)
